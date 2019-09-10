0ae61e50bc1195b92150e94738b1c20f7be4561b
Fernanda Vitorino <fernandavitorino3@gmail.com>
2019-09-10 17:33:02 -0300

    chore: method put and delete

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const projects = [
   {
     id: "1",
     title: "Novo projeto",
-    tasks: ["Nova tarefa"]
+    tasks: ["Tarefa 1 proj. 1", "Tarefa 2 proj. 1"]
   },
   {
     id: "2",
     title: "Novo projeto 2",
-    tasks: ["Nova tarefa 2"]
+    tasks: ["Tarefa 1 proj. 2", "Tarefa 2 proj. 2"]
   }
 ];
 
@@ -21,10 +21,10 @@ server.get("/projects", (req, res) => {
   return res.json(projects);
 });
 
-server.get("/projects/:index", (req, res) => {
-  const { index } = req.params;
+server.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
 
-  return res.json(projects[index]);
+  return res.json(projects[id]);
 });
 
 server.post("/projects", (req, res) => {
@@ -38,6 +38,25 @@ server.post("/projects", (req, res) => {
   };
 
   projects.push(project);
+  return res.json(projects);
+});
+
+server.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
   return res.json(projects);
 });
 
